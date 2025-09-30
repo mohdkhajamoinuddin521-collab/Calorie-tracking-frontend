b68b5c5138c8988f43215ad1a85944de655e33e0
MOHD KHAJA <mohdkhaja.moinuddin521@gmail.com>
2025-09-30 11:54:09 +0530

    api commit

--- src/api.js
+++ src/api.js
@@ -3,6 +3,11 @@ import axios from 'axios';
 const isDevelopment = import.meta.env.MODE === 'development'
 const myBaseURL = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOY;
 
+console.log("MODE:", import.meta.env.MODE);
+console.log("LOCAL:", import.meta.env.VITE_API_BASE_URL_LOCAL);
+console.log("DEPLOY:", import.meta.env.VITE_API_BASE_URL_DEPLOY);
+console.log("Using baseURL:", myBaseURL);
+
 const api = axios.create({
   baseURL: myBaseURL,
 });
